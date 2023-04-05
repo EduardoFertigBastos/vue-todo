eefbdedeb790529767a862facc33875ae06e36fa
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2023-04-05 15:24:00 -0300

    Todo Page

---
+++ src/views/HomeTodo.vue
@@ -0,0 +1,38 @@
+<script setup>
+import TodoList from '../components/TodoList.vue'
+</script>
+
+<template>
+  <main>
+    <TodoList :preTasks="preTasks" />
+  </main>
+</template>
+
+<script>
+import { v4 as uuidv4 } from 'uuid';
+
+export default {
+  name: 'HomeTodo',
+  data() {
+    return {
+      preTasks: [
+        {
+          id: uuidv4(),
+          title: 'Task 1',
+          completed: false
+        },
+        {
+          id: uuidv4(),
+          title: 'Task 2',
+          completed: false
+        },
+        {
+          id: uuidv4(),
+          title: 'Task 3',
+          completed: false
+        }
+      ]
+    }
+  }
+}
+</script>
