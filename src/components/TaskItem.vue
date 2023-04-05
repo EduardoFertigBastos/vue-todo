5252ec91a275bd60a8981c01ef30080cc6b0880e
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2023-04-05 15:24:15 -0300

    TaskItem

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <li>
+    <button @click="$emit('completeTask', task.id)"
+      :class="className">
+      <i class="far fa-circle"></i>
+      {{ task.title }}
+    </button>
+
+    <div class="buttonGroup">
+      <button @click="$event => $emit('editTask', task.id)">
+        <i class="fa-regular fa-pen-to-square"></i>
+      </button>
+  
+      <button @click="$event => $emit('removeTask', task.id)">
+        <i class="far fa-trash-alt"></i>
+      </button>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'TaskItem',
+  props: {
+    task: Object
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    className() {
+      let classes = ['toggle']
+
+      if (this.task.completed) {
+        classes.push('toggle-completed')
+      }
+
+      return classes
+    }
+  }
+}
+</script>
